4db757b54bcc76a1bdac976975435e0165ad8443
Nikita Emelin <WhisperQ@Nikitas-MacBook-Pro.local>
2018-04-03 21:50:04 +0300

    Shop begin
    Shop begin

---
+++ Shop/Models/AddItemModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models
+{
+    public class AddItemModel
+    {
+			[Required]
+            public string name { get; set; }
+			[Required]
+			public string description { get; set; }
+			[Required]
+			public string type_item { get; set; }
+			[Required]
+            public bool is_avalible { get; set; }
+			[Required]
+            public float cost { get; set; }
+			[Required]
+            public string image { get; set; }
+    
+    }
+}
