bd42cfa53e7ba826332128ea72cd19b22915ae1f
Emelin Nikita <clever899@gmail.com>
2018-04-04 18:51:23 +0300

    Add Item ready partical

--- Shop/Controllers/ShopController.cs
+++ Shop/Controllers/ShopController.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Context;
 using Shop.Data;
+using Shop.Models;
 
 namespace Shop.Controllers
 {
@@ -23,10 +22,24 @@ namespace Shop.Controllers
             return View(items);
         }
 
+        [HttpGet]
         public async Task<IActionResult> Add()
         {
             
             return View();
+        }
+
+        public async Task<IActionResult> Add(AddItemModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Item item = new Item {name = model.name, description = model.description,type_item = model.type_item, is_avalible = model.is_avalible,cost = model.cost,image = model.image };
+                await _context.Items.AddAsync(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+                
+            }
+            return View(model);
         }
     }
 }
