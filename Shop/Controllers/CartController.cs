a52c9cb9865edb9a97d781ed07ecd116297d0f67
Nikita Emelin <n.emelin@takewing.ru>
2018-05-28 19:42:48 +0300

    Cart V3

--- Shop/Controllers/CartController.cs
+++ Shop/Controllers/CartController.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Shop.Data;
+using Shop.Models;
 
 namespace Shop.Controllers
 {
     public class CartController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            Cart cart_order = null;
+            string cart_json = "";
+            float sum = 0;
+            CartModel model = new CartModel();
+            if (Request.Cookies.ContainsKey("cart"))
+            {
+                cart_json = Request.Cookies["cart"];
+                cart_order = JsonConvert.DeserializeObject<Cart>(cart_json);
+            }
+            foreach (Item value in cart_order.items)
+            {
+                sum = value.cost + sum;
+            }
+            model.cart = cart_order;
+            model.total_cost =Convert.ToInt32(sum);
+
+            return View(model);
         }
     }
 }
--- Shop/Models/CartModel.cs
+++ Shop/Models/CartModel.cs
@@ -3,18 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Shop.Data;
 
 namespace Shop.Models
 {
     public class CartModel
     {
-        [Required]
-        public string name { get; set; }
-        [Required]
-        public string description { get; set; }
-        [Required]
-        public float cost { get; set; }
-        [Required]
-        public string image { get; set; }
+        public Cart cart { get; set; }
+        public int total_cost { get; set; }
     }
 }
